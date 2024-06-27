7f40015ec9379756830fa4742323cc3bc00de75f
sae-kumar <saekumar56@gmail.com>
2024-06-27 16:04:03 +0530

    added

--- Basic_programs/primeNum.js
+++ Basic_programs/primeNum.js
@@ -1,18 +1,18 @@
-// function isprime(num) {
-//   if (num <= 1) {
-//     return false;
-//   }
-//   for (let i = 2; i < num; i++) {
-//     if (num % i === 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-// ///
-// let number = 2;
-// if (isprime(number)) {
-//   console.log(number + "is a prime number");
-// } else {
-//   console.log(number + "is not a prime number");
-// }
+function isprime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+///
+let number = 2;
+if (isprime(number)) {
+  console.log(number + "is a prime number");
+} else {
+  console.log(number + "is not a prime number");
+}
