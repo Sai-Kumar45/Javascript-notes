a878c9f8dafadf6fe6994af7ff52f785585f7d88
Sai-Kumar45 <saekumar56@gmail.com>
2024-07-11 21:38:48 +0530

    fourth commit

--- 30_programs/program4.js
+++ 30_programs/program4.js
@@ -1,2 +1,13 @@
 // 4. Write a JavaScript program to count number of duplicates elements of an array
-var arr = [];
+var arr = [2, 5, 7, 9, 3, 2, 5, 21, 9, 14, 17, 22, 21];
+var duplicates = [];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] === arr[j]) {
+      duplicates.push(arr[i]);
+    }
+  }
+}
+
+console.log(duplicates);
